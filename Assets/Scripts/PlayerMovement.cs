fd35ffaebd06980746805aff99c06313de212818
pruthvi10 <pruthvimohanlal10@gmail.com>
2023-03-07 21:43:10 +0000

    added a cube with movement in 3d

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        rb.velocity = movement * speed;
+    }
+}
