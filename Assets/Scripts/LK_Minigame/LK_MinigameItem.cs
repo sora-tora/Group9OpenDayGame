534d7fe52732fea4eefcfd43bb3f98585d90ea87
Logan King <65763483+loganator956@users.noreply.github.com>
2023-05-06 10:43:13 +0100

    organise project

---
+++ Assets/Scripts/LK_Minigame/LK_MinigameItem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minigames.Logan.Minigame1
+{
+    [System.Serializable]
+    public class LK_MinigameItem
+    {
+        public string Name;
+        public GameObject SpawnableObject;
+    }
+}
---
+++ Assets/Scripts/LK_Minigame/LK_MinigameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minigames.Logan.Minigame1
+{
+    public class LK_MinigameManager : MonoBehaviour
+    {
+        public int Score = 0;
+        public float RemainingTime = 50f;
+        public
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
